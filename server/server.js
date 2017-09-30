73e736c37f752764ccd9e252a9cf11d2eddae88f
Miguel Vacas <m.vacas@agenciachat.com>
2017-09-30 18:30:06 -0400

    Adding compression middleware for express in server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '../views')));
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true }));
-// app.use(compression());
+app.use(compression());
 
 // Handlebars config
 var hbs = exphbs.create({
